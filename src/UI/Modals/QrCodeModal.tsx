010c4ea49c657ba4e9c74ebb662ae8834be7f868
Abdul Aleem Mughal <137313559+AbdulaleemMughal@users.noreply.github.com>
2026-03-19 14:57:47 +0500

    Qr Code Generator complete

---
+++ src/UI/Modals/QrCodeModal.tsx
@@ -0,0 +1,99 @@
+import { Box, Modal } from "@mui/material";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../store/appStore";
+import { X } from "lucide-react";
+import QRCode from "react-qr-code";
+import { Button } from "../Button";
+import type React from "react";
+import { useRef } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "90%",
+  maxWidth: 800,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  outline: "none",
+  overflowY: "auto",
+};
+
+interface QrCodeModalProps {
+  link: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const QrCodeModal = ({ link, open, setOpen }: QrCodeModalProps) => {
+  const qrRef = useRef<HTMLDivElement | null>(null);
+  const accentColor = useSelector(
+    (store: RootState) => store.memorial.accentColor,
+  );
+
+  const handleQrDownload = () => {
+    const svg = qrRef.current?.querySelector("svg");
+    if (!svg) return;
+
+    const serializer = new XMLSerializer();
+    const svgString = serializer.serializeToString(svg);
+
+    const img = new Image();
+    const svgBlob = new Blob([svgString], {
+      type: "image/svg+xml;charset=utf-8",
+    });
+    const url = URL.createObjectURL(svgBlob);
+
+    img.onload = () => {
+      const padding = 40;
+
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      canvas.width = img.width + padding * 2;
+      canvas.height = img.height + padding * 2;
+
+      ctx!.fillStyle = "#ffffff";
+      ctx!.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx?.drawImage(img, padding, padding);
+
+      const png = canvas.toDataURL("image/png");
+
+      const downloadLink = document.createElement("a");
+      downloadLink.href = png;
+      downloadLink.download = "qr-code.png";
+      downloadLink.click();
+
+      URL.revokeObjectURL(url);
+    };
+
+    img.src = url;
+  };
+  return (
+    <Modal open={open} onClose={() => setOpen(false)}>
+      <Box sx={style}>
+        <div className="p-4 flex items-center justify-between border-b border-gray-300">
+          <h1
+            className="text-2xl font-[Spectral]"
+            style={{ color: accentColor }}
+          >
+            Scan or Download your Qr Code.
+          </h1>
+          <span className="cursor-pointer" onClick={() => setOpen(false)}>
+            <X color="gray" size={28} strokeWidth={2} />
+          </span>
+        </div>
+        <div className="py-5 px-6.25">
+          <div ref={qrRef} className="flex justify-center">
+            <QRCode value={link} level="H" />
+          </div>
+          <div className="mt-8 flex justify-center">
+            <Button text="Download Qr Code" onClick={handleQrDownload} />
+          </div>
+        </div>
+      </Box>
+    </Modal>
+  );
+};
--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -11,6 +11,8 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import type { MemorialType } from "../../@types/memorial.type";
 import { useMemorial } from "../../hook/useMemorial";
 import { handleScroll } from "../../utils/pageScroll";
+import { QrCodeModal } from "../../UI/Modals/QrCodeModal";
+import { useState } from "react";
 
 interface HeaderProps {
   memorialData: MemorialType;
@@ -20,8 +22,7 @@ export const Header = ({ memorialData }: HeaderProps) => {
   const { memorialId } = useParams();
   const navigate = useNavigate();
   const { deleteMemorial } = useMemorial();
-
-  
+  const [openQrModal, setOpenQrMOdal] = useState<boolean>(false);
 
   const handleDeletePage = async () => {
     if (memorialId) {
@@ -32,62 +33,72 @@ export const Header = ({ memorialData }: HeaderProps) => {
   };
 
   return (
-    <div className="my-5 mx-auto max-w-7xl bg-white">
-      <div className="p-7 flex items-center gap-8 justify-between overflow-x-auto whitespace-nowrap flex-nowrap custom-scrollbar">
-        <div onClick={() => handleScroll("dashboard")}>
-          <div className="flex items-center gap-3 cursor-pointer shrink-0">
-            <Info size={18} strokeWidth={1.5} color="#a07ae2" />
+    <>
+      <div className="my-5 mx-auto max-w-7xl bg-white">
+        <div className="p-7 flex items-center gap-8 justify-between overflow-x-auto whitespace-nowrap flex-nowrap custom-scrollbar">
+          <div onClick={() => handleScroll("dashboard")}>
+            <div className="flex items-center gap-3 cursor-pointer shrink-0">
+              <Info size={18} strokeWidth={1.5} color="#a07ae2" />
+              <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
+                Dashboard
+              </p>
+            </div>
+          </div>
+
+          <div onClick={() => handleScroll("statistics")}>
+            <div className="flex items-center gap-3 cursor-pointer shrink-0">
+              <ChartNoAxesColumn size={18} strokeWidth={1.5} color="#a07ae2" />
+              <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
+                Statistics
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="flex items-center gap-3 cursor-pointer shrink-0"
+            onClick={handleDeletePage}
+          >
+            <Trash size={18} strokeWidth={1.5} color="#a07ae2" />
             <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
-              Dashboard
+              Delete Page
             </p>
           </div>
-        </div>
 
-        <div onClick={() => handleScroll("statistics")}>
-          <div className="flex items-center gap-3 cursor-pointer shrink-0">
-            <ChartNoAxesColumn size={18} strokeWidth={1.5} color="#a07ae2" />
+          <div
+            className="flex items-center gap-3 cursor-pointer shrink-0"
+            onClick={() => setOpenQrMOdal(true)}
+          >
+            <QrCode size={18} strokeWidth={1.5} color="#a07ae2" />
             <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
-              Statistics
+              Download QR Code
             </p>
           </div>
-        </div>
 
-        <div
-          className="flex items-center gap-3 cursor-pointer shrink-0"
-          onClick={handleDeletePage}
-        >
-          <Trash size={18} strokeWidth={1.5} color="#a07ae2" />
-          <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
-            Delete Page
-          </p>
-        </div>
-
-        <div className="flex items-center gap-3 cursor-pointer shrink-0">
-          <QrCode size={18} strokeWidth={1.5} color="#a07ae2" />
-          <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
-            Download QR Code
-          </p>
-        </div>
-
-        <Link to={`/memorial/${memorialId}`} target="_blank">
-          <div className="flex items-center gap-3 cursor-pointer shrink-0">
-            <Settings size={18} strokeWidth={1.5} color="#a07ae2" />
+          <Link to={`/memorial/${memorialId}`} target="_blank">
+            <div className="flex items-center gap-3 cursor-pointer shrink-0">
+              <Settings size={18} strokeWidth={1.5} color="#a07ae2" />
+              <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
+                Edit Page
+              </p>
+            </div>
+          </Link>
+          <Link
+            to={`/live/${memorialData?._id}`}
+            target="_blank"
+            className="flex items-center gap-3 cursor-pointer shrink-0"
+          >
+            <ExternalLink size={18} strokeWidth={1.5} color="#a07ae2" />
             <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
-              Edit Page
+              View Page
             </p>
-          </div>
-        </Link>
-        <Link
-          to={`/live/${memorialData?._id}`}
-          target="_blank"
-          className="flex items-center gap-3 cursor-pointer shrink-0"
-        >
-          <ExternalLink size={18} strokeWidth={1.5} color="#a07ae2" />
-          <p className="text-[16px] font-[Poppins] text-(--secondary-color)">
-            View Page
-          </p>
-        </Link>
+          </Link>
+        </div>
       </div>
-    </div>
+      <QrCodeModal
+        link={`${window.location.origin}/live/${memorialId}`}
+        open={openQrModal}
+        setOpen={setOpenQrMOdal}
+      />
+    </>
   );
 };
